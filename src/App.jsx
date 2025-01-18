6e8480515a1bf4f671cb0308ed791c164d285916
Corey Skidmore <skidmoreco@gmail.com>
2025-01-18 12:44:55 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
-import Main from "./components/main"
+import Main from "./components/Main"
 import SideBar from "./components/Sidebar"
 
 function App() {
